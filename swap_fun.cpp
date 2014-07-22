6ab23241e1e2ca1b7b1b2b72c40c0e9b4e696bae
kevin-lwb <liuwbcn@gmail.com>
2014-07-22 22:31:11 +0800

    add linear search algorithm

--- swap_fun.cpp
+++ swap_fun.cpp
@@ -1,6 +1,7 @@
 //版本1：交换两个int类型变量值
 
-void swap(int *ap, int *bp){
+void swap(int *ap, int *bp)
+{
 	int temp = *ap;
 	*ap = *bp;
 	*bp = temp;
@@ -8,7 +9,8 @@ void swap(int *ap, int *bp){
 
 //版本2，通用交换变量值
 
-void swap(void *vp1, void *vp2, int size){
+void swap(void *vp1, void *vp2, int size)
+{
 	char* buffer[size];
 	/***
 	memcpy(void *p1, void *p2, int size);
@@ -22,3 +24,76 @@ void swap(void *vp1, void *vp2, int size){
 	memcpy(vp1, vp2, size);
 	memcpy(vp2, buffer,  size);
 }
+
+Example:
+char* husband *strdup("Fred");
+char* wife *strdup("Wilma");
+
+//use like this:
+swap(& husband, &wife, sizeof(char*)); //交换指针变量的值即地址
+
+//do not use like this:
+swap(husband, wife, sizeof(char*)); //交换的是F和W,即指针所指向的内容(*p)
+
+//线性搜索算法
+//版本1: 搜索某个int类型值key在int类型数组中的索引，
+int lsearch(int key, int arry[], int size)
+{	
+	for(int i=0; i<size; i++){
+		if(arry[i] == key){
+			return i;
+		}
+	}
+	return -1;
+}
+
+//版本2: 通用版本
+/*	key:搜索值
+	base:数组地址
+	n:数组大小
+	elemSize:元素大小
+*/
+void *lsearch(void* key, void* base, int n, int elemSize)
+{
+	for(int i=0; i<n; i++){
+		void *elmeAddr = (char*)base + i*elemSize;
+		if(memcmp(key, elemAddr, elemSize) == 0){
+			return elemAddr;
+		}
+	}
+	return NULL;	
+}
+//版本3: 第五个参数
+
+void *lsearch( void* key, void* base, int n, int elemSize,
+			  int* cpmFn(void*, void*) )
+{
+	for(int i=0; i<n; i++){
+		void *elmeAddr = (char*)base + i*elemSize;
+		if(comFn(key, elemAddr) == 0){
+			return elemAddr;
+		}
+	}
+	return NULL;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
